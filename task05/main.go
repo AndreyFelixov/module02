1274e2199eafad315c2951c5ac6074de15b3b00a
AndreyFelixov <andreyokay1337@gmail.com>
2024-08-13 14:19:27 +0300

    func task

--- task05/main.go
+++ task05/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 )
 
 func main() {
-	fmt.Print("Hello, world!")
+	a := make([]string, 0, 5)
+	a = append(a, "Hello", "my", "name", "is", "Andrey")
+	x := " "
+	fmt.Println(Contains(a, x))
+	fmt.Println(getMax(3, 4, 52, 9, -232, 1, 3, 5, 6, 7))
+}
+
+func Contains(a []string, x string) bool {
+	b := strings.Join(a, " ")
+	return strings.Contains(b, x)
+
+}
+func getMax(c ...int) int {
+	sort.Ints(c)
+	return c[len(c)-1]
 }
