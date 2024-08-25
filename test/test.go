8dd415fd16610e678ac2311184b2e1e8c19bfa99
AndreyFelixov <andreyokay1337@gmail.com>
2024-08-25 18:38:23 +0300

    7final

--- test/test.go
+++ test/test.go
@@ -8,75 +8,60 @@ import (
 	"strings"
 )
 
-var rows = make([]string, 0)
+type Row struct {
+	Name    string
+	Address string
+	City    string
+}
+
 var pntr *int
 
 func main() {
-	pathIn := "D:/projects/module02/task07/data/in.txt"
-	pathOut := "D:/projects/module02/task07/data/out.txt"
-	getFile(pathIn, pathOut)
+	path := "data/in777.txt"
+	path1 := "data/out.txt"
+	getFile(path, path1)
 }
-
-func getFile(pathIn, pathOut string) {
-	flag := false
-	fileIn, _ := os.Open(pathIn)
-	s := bufio.NewScanner(fileIn)
-	var strCounter int
-	for s.Scan() {
-		strCounter += 1
-		if !(strings.Contains(s.Text(), "||") || string(s.Text()[0]) == "|" || string(s.Text()[len(s.Text())-1]) == "|") {
-			rows = append(rows, s.Text())
+func getFile(path, path1 string) {
+	rowsFile, _ := os.ReadFile(path)
+	file, _ := os.Create("data/out.txt")
+	writer := bufio.NewWriter(file)
+	defer file.Close()
+	rowLines := strings.Split(string(rowsFile), "\n")
+	fmt.Print(len(rowLines))
+	for i := 0; i < len(rowLines); i++ {
+		rowLine := strings.Split(string(rowLines[i]), "|")
+		newRow := Row{Name: rowLine[0], Address: rowLine[1], City: rowLine[2]}
+		if newRow.Name != "" && newRow.Address != "" && newRow.City != "" {
+			writer.WriteString(strconv.Itoa(i + 1))
+			writer.WriteString("\n")
+			writer.WriteString(newRow.Name)
+			writer.WriteString("\n")
+			writer.WriteString(newRow.Address)
+			writer.WriteString("\n")
+			writer.WriteString(newRow.City)
+			writer.WriteString("\n\n\n")
 		} else {
-			flag = true
-			pntr = &strCounter
-			break
+			outFile, _ := os.ReadFile(path1)
+			fmt.Print(string(outFile))
+			pntr = &i
 		}
+		writer.Flush()
 	}
-	fileIn.Close()
-	if !flag {
-		writeAndPrint(pathOut)
-	} else {
-		writeAndPrint(pathOut)
-		err := panicFunc()
-		if err != nil {
-			fmt.Println(err)
-		}
+	err := someFunc()
+	if err != nil {
+		fmt.Println(err)
 	}
 }
-
-func panicFunc() (err error) {
+func someFunc() (err error) {
 	defer func() {
 		if panicErr := recover(); panicErr != nil {
 			switch panicErr {
 			case "parse error":
-				err = fmt.Errorf("parse error: empty field in %dth string", *pntr)
+				err = fmt.Errorf("parse error: empty field in %dth user", *pntr+1)
 			default:
 				panic("critical")
 			}
 		}
 	}()
 	panic("parse error")
-}
-
-func writeAndPrint(pathOut string) {
-	fileOut, _ := os.Create(pathOut)
-	w := bufio.NewWriter(fileOut)
-	for i := 0; i < len(rows); i++ {
-		rowLine := strings.Split(rows[i], "|")
-		w.WriteString(strconv.Itoa(i + 1))
-		w.WriteString("\n")
-		w.WriteString(rowLine[0])
-		w.WriteString("\n")
-		w.WriteString(rowLine[1])
-		w.WriteString("\n")
-		w.WriteString(rowLine[2])
-		w.WriteString("\n\n\n")
-	}
-	w.Flush()
-	fileOut, _ = os.Open(pathOut)
-	defer fileOut.Close()
-	s := bufio.NewScanner(fileOut)
-	for s.Scan() {
-		fmt.Println(s.Text())
-	}
 }
